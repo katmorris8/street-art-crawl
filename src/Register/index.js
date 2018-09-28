259d74fd4abd33793572c1daa02a78364c2806b3
WadeSeidule <wade.seidule@gmail.com>
2018-09-28 10:51:00 -0400

    register works from frontend + added begun login

--- src/Register/index.js
+++ src/Register/index.js
@@ -7,6 +7,9 @@ export default class Register extends Component {
         super(props)
 
         this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
             username: '',
             password: '',
             errorMessage: ''
@@ -15,6 +18,9 @@ export default class Register extends Component {
 
     register = async () => {
         const requestBody = JSON.stringify({
+            firstName: this.state.firstName,  
+            lastName: this.state.lastName,  
+            email: this.state.email,
             username: this.state.username,
             password: this.state.password,
         });
@@ -37,11 +43,39 @@ export default class Register extends Component {
     }
 
 
+    logIn = async () => {
+        const requestBody = JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        });
+        const response = await fetch('/api/login', {
+          method: 'POST',
+          body: requestBody,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const responseBody = await response.json();
+        if (response.status === 401) {
+          this.setState({
+            errorMessage: responseBody.message
+          });
+          return;
+        }
+        // this.props.onLogIn();
+        localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
+      }
+
+
 
     submitHandler = (e) => {
         e.preventDefault();
-        this.register();
+    }
 
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
@@ -53,13 +87,14 @@ export default class Register extends Component {
                     </Link>
                     <h1>Register</h1>
                     <form onSubmit={this.submitHandler} >
-                        <input value={this.state.value} type="text" placeholder='First Name' />
-                        <input value={this.state.value} type="text" placeholder='Last Name' />
-                        <input value={this.state.value} type="text" placeholder='Email Address' />
-                        <input value={this.state.value} type="text" placeholder='Username' />
-                        <input value={this.state.value} type="text" placeholder='Password' />
-                        <input value={this.state.value} type="text" placeholder='Confirm Password' />
-                        <input type="submit" value="submit" />
+                        <input value={this.state.firstName} onChange={this.onInputChange} type="text" placeholder='First Name' name='firstName' />
+                        <input value={this.state.lastName} onChange={this.onInputChange} type="text" placeholder='Last Name' name='lastName' />
+                        <input value={this.state.email} onChange={this.onInputChange} type="text" placeholder='Email Address'name='email' />
+                        <input value={this.state.username} onChange={this.onInputChange} type="text" placeholder='Username'name='username' />
+                        <input value={this.state.password} onChange={this.onInputChange} type="text" placeholder='Password'name='password' />
+                        {/* <input value={this.state.password} type="text" placeholder='Confirm Password' /> */}
+                        <button type="button" onClick={this.register}>Register</button>
+                        <button type="button" onClick={this.logIn}>Login</button>
                     </form>
                 </div>
             
