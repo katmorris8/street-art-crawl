d27c575f92257461a5efa8135b1e2251aae525f6
WadeSeidule <wade.seidule@gmail.com>
2018-09-28 11:58:22 -0400

    started login

--- src/App/index.js
+++ src/App/index.js
@@ -15,6 +15,7 @@ class App extends Component {
       lat: 51.505,
       lng: -0.09,
       zoom: 15,
+      isLoggedIn: false
     }
   }
 
@@ -29,13 +30,19 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition(this.showPosition);
   }
 
+  getLoggedIn = () => {
+    this.setState({
+      isLoggedIn: true
+    })
+  }
+
   render() {
     return (
 
       <div className="App">
         <Router>
           <div className='nav-container'>
-            {!this.props.loggedIn &&
+            {!this.state.isLoggedIn &&
               <div>
                 <Link to='/register'>
                   <button className='register-btn'>Register</button>
@@ -43,7 +50,7 @@ class App extends Component {
                 <button className='login-btn'>Login</button>
               </div>
             }
-            {this.props.loggedIn &&
+            {this.state.isLoggedIn &&
               <nav className='nav-bar homepage-nav'>
                 <Link to='/'>Home</Link>
                 &nbsp;
@@ -51,7 +58,7 @@ class App extends Component {
                 &nbsp;
                 <Link to='/profile'>Profile</Link>
               </nav>}
-            <Route path="/register" exact component={Register}/>
+            <Route path="/register" render={(props) => <Register {...props} getLoggedIn={this.getLoggedIn}/>}/>
             <Route path="/" exact component={HomePage} />
             <Route path="/map" render={(props) => <MapPage {...props} lat={this.state.lat} long={this.state.lng} zoom={this.state.zoom}/>} />
             <Route path="/profile" exact component={Profile} />
--- src/Register/index.js
+++ src/Register/index.js
@@ -18,8 +18,8 @@ export default class Register extends Component {
 
     register = async () => {
         const requestBody = JSON.stringify({
-            firstName: this.state.firstName,  
-            lastName: this.state.lastName,  
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
             email: this.state.email,
             username: this.state.username,
             password: this.state.password,
@@ -38,33 +38,33 @@ export default class Register extends Component {
             });
             return;
         }
-        //   this.props.onLogIn();
+        this.props.getLoggedIn();
         localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
     }
 
 
     logIn = async () => {
         const requestBody = JSON.stringify({
-          username: this.state.username,
-          password: this.state.password,
+            username: this.state.username,
+            password: this.state.password,
         });
         const response = await fetch('/api/login', {
-          method: 'POST',
-          body: requestBody,
-          headers: {
-            'Content-Type': 'application/json'
-          }
+            method: 'POST',
+            body: requestBody,
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
         const responseBody = await response.json();
         if (response.status === 401) {
-          this.setState({
-            errorMessage: responseBody.message
-          });
-          return;
+            this.setState({
+                errorMessage: responseBody.message
+            });
+            return;
         }
-        // this.props.onLogIn();
+        this.props.getLoggedIn();
         localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
-      }
+    }
 
 
 
@@ -80,24 +80,24 @@ export default class Register extends Component {
 
     render() {
         return (
-        
-                <div>
-                    <Link to='/'>
-                        <button className='register-back-btn'>Back</button>
-                    </Link>
-                    <h1>Register</h1>
-                    <form onSubmit={this.submitHandler} >
-                        <input value={this.state.firstName} onChange={this.onInputChange} type="text" placeholder='First Name' name='firstName' />
-                        <input value={this.state.lastName} onChange={this.onInputChange} type="text" placeholder='Last Name' name='lastName' />
-                        <input value={this.state.email} onChange={this.onInputChange} type="text" placeholder='Email Address'name='email' />
-                        <input value={this.state.username} onChange={this.onInputChange} type="text" placeholder='Username'name='username' />
-                        <input value={this.state.password} onChange={this.onInputChange} type="text" placeholder='Password'name='password' />
-                        {/* <input value={this.state.password} type="text" placeholder='Confirm Password' /> */}
-                        <button type="button" onClick={this.register}>Register</button>
-                        <button type="button" onClick={this.logIn}>Login</button>
-                    </form>
-                </div>
-            
+
+            <div>
+                <Link to='/'>
+                    <button className='register-back-btn'>Back</button>
+                </Link>
+                <h1>Register</h1>
+                <form onSubmit={this.submitHandler} >
+                    <input value={this.state.firstName} onChange={this.onInputChange} type="text" placeholder='First Name' name='firstName' />
+                    <input value={this.state.lastName} onChange={this.onInputChange} type="text" placeholder='Last Name' name='lastName' />
+                    <input value={this.state.email} onChange={this.onInputChange} type="text" placeholder='Email Address' name='email' />
+                    <input value={this.state.username} onChange={this.onInputChange} type="text" placeholder='Username' name='username' />
+                    <input value={this.state.password} onChange={this.onInputChange} type="text" placeholder='Password' name='password' />
+                    {/* <input value={this.state.password} type="text" placeholder='Confirm Password' /> */}
+                    <button type="button"  onClick={this.register}>Register</button>
+                    <button type="button" onClick={this.logIn}>Login</button>
+                </form>
+            </div>
+
         )
     }
 }
