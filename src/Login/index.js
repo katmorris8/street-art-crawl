b06ec252fbaa637b7b4d41882d59e42372353592
WadeSeidule <wade.seidule@gmail.com>
2018-09-28 15:38:35 -0400

    finished registration and login

--- src/Login/index.js
+++ src/Login/index.js
@@ -6,77 +6,75 @@ import HomePage from '../HomePage';
 
 
 class Login extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-        username: '',
-        password: '',
-        errorMessage: ''
+        this.state = {
+            username: '',
+            password: '',
+            errorMessage: ''
+        }
     }
-}
 
 
+    logIn = async () => {
+        const requestBody = JSON.stringify({
+            username: this.state.username,
+            password: this.state.password,
+        });
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: requestBody,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const responseBody = await response.json();
+        if (response.status === 401) {
+            this.setState({
+                errorMessage: responseBody.message
+            });
+            return;
+        }
+        this.props.getLoggedIn();
+        localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
+    }
 
+    submitHandler = (e) => {
+        e.preventDefault();
+    }
 
-logIn = async () => {
-  const requestBody = JSON.stringify({
-      username: this.state.username,
-      password: this.state.password,
-  });
-  const response = await fetch('/api/login', {
-      method: 'POST',
-      body: requestBody,
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  });
-  const responseBody = await response.json();
-  if (response.status === 401) {
-      this.setState({
-          errorMessage: responseBody.message
-      });
-      return;
-  }
-  this.props.getLoggedIn();
-  localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
-}
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
-submitHandler = (e) => {
-  e.preventDefault();
-}
+    render() {
+        console.log(this.props.isLoggedIn);
+        if (this.props.isLoggedIn) {
+            const { from } = this.props.location.state || { from: { pathname: "/" } };
+            return (
+                <Redirect to={from} />
+            )
+        }
+        return (
 
-onInputChange = (e) => {
-  this.setState({
-      [e.target.name]: e.target.value
-  })
-}
+            <div>
+                <Link to='/'>
+                    <button className='login-back-btn'>Back</button>
+                </Link>
+                <h1>Login</h1>
+                <form onSubmit={this.submitHandler} >
+                    <input value={this.state.username} onChange={this.onInputChange} type="text" placeholder='Username' name='username' />
+                    <input value={this.state.password} onChange={this.onInputChange} type="text" placeholder='Password' name='password' />
+                    <button type="button" onClick={this.logIn}>Login</button>
+                </form>
+                <PrivateRoute path='/' exact component={HomePage} />
+            </div>
 
-render() {
-  if (this.props.isLoggedIn){
-    const { from } = this.props.location.state || {from: { pathname: "/"} };
-    return(
-      <Redirect to={from}/>
-    )
-  }
-  return (
-
-      <div>
-          <Link to='/'>
-              <button className='login-back-btn'>Back</button>
-          </Link>
-          <h1>Login</h1>
-          <form onSubmit={this.submitHandler} >
-              <input value={this.state.username} onChange={this.onInputChange} type="text" placeholder='Username' name='username' />
-              <input value={this.state.password} onChange={this.onInputChange} type="text" placeholder='Password' name='password' />
-              <button type="button" onClick={this.register}>Register</button>
-              <button type="button" onClick={this.logIn}>Login</button>
-          </form>
-          <PrivateRoute path='/' exact component={HomePage}/>
-      </div>
-
-  )
-}
+        )
+    }
 }
 
 export default Login;
