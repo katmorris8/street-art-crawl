7f39aeac9f2ccc3d553dd83a372a7e7e9147b8a4
Kat Morris <kathryn.leigh.morris@gmail.com>
2018-10-01 11:48:30 -0400

    adjusted models file

--- models.js
+++ models.js
@@ -9,26 +9,26 @@ const User = sequelize.define('user', {
   firstName: Sequelize.TEXT,
   lastName: Sequelize.TEXT,
   email: Sequelize.TEXT,
-  username:{
-    type:Sequelize.TEXT,
-    unique:true
+  username: {
+    type: Sequelize.TEXT,
+    unique: true
   }, 
   passwordDigest: Sequelize.TEXT,
   userScore: Sequelize.INTEGER,
-  icon:Sequelize.TEXT,
-  art_id:Sequelize.INTEGER
+  icon: Sequelize.TEXT,
+  art_id: Sequelize.INTEGER
 });
 
 const Art = sequelize.define('art', {
   neighborhood: Sequelize.TEXT,
   location: Sequelize.TEXT,
   date: Sequelize.DATEONLY,
-  description:Sequelize.TEXT,
-  posterPath:Sequelize.TEXT,
-  user_id:Sequelize.INTEGER
+  description: Sequelize.TEXT,
+  posterPath: Sequelize.TEXT,
+  user_id: Sequelize.INTEGER
 });
 
-Art.hasMany(User);
+Art.hasMany(User, {onDelete: 'cascade'});
 User.belongsTo(Art);
 
 module.exports = {
